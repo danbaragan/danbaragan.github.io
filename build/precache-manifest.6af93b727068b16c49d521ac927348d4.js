ef761904c742c3101e0a703904f523b80ffc5e24
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2020-06-09 15:28:33 +0300

    Commit build

---
+++ build/precache-manifest.6af93b727068b16c49d521ac927348d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc9f9fc6b639683b3238485639090d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd43fe87a815b46d4112",
+    "url": "/static/css/main.5bd06cbd.chunk.css"
+  },
+  {
+    "revision": "2694e2897ca837a4cc63",
+    "url": "/static/js/2.43ccebdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43ccebdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd43fe87a815b46d4112",
+    "url": "/static/js/main.ed1338f5.chunk.js"
+  },
+  {
+    "revision": "7291bcc96f171b3073fc",
+    "url": "/static/js/runtime-main.b0108151.js"
+  }
+]);
