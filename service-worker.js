71225f82ef40ca35b82dd51e4a6a7a92de27b0c5
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2020-06-19 19:41:43 +0300

    Fix responsive design for smaller screens

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6af93b727068b16c49d521ac927348d4.js"
+  "/precache-manifest.724ab42e99129f19f509905ad6e9c9dc.js"
 );
 
 self.addEventListener('message', (event) => {
