23ee6d58ea151a90e96a3f7f6b37418a72c72246
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2020-10-12 13:46:26 +0300

    Better links; internal and external

--- build/precache-manifest.2ba971810c83b5b89257033d0beae60e.js
+++ build/precache-manifest.2ba971810c83b5b89257033d0beae60e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "50a581fd90fabfa5a2fc378578c79762",
+    "revision": "a4647d656843cc65eba3e82becaaf2d9",
     "url": "/index.html"
   },
   {
-    "revision": "ef98cc76310f55edb7f0",
-    "url": "/static/css/main.f2f04e45.chunk.css"
+    "revision": "133ac325682acd9c4ab0",
+    "url": "/static/css/main.be91d957.chunk.css"
   },
   {
     "revision": "2694e2897ca837a4cc63",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.43ccebdd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ef98cc76310f55edb7f0",
+    "revision": "133ac325682acd9c4ab0",
     "url": "/static/js/main.ed1338f5.chunk.js"
   },
   {
